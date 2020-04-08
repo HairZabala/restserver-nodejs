57d3bcb8f939327fbf42ae22ecdd93ce34673f70
Hair <hair.zabala@gmail.com>
2020-04-08 22:08:22 +1000

    Heroku variable de entorno DB

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://hair-zabala:NRXEgSusGP9oEmsT@cluster0-2m1np.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
